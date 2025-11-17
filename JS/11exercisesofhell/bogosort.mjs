6ba03d96d56440de17d4b527e3b1a19a3b404eb3
Cherlyte <dogchase4@hotmail.com>
2025-11-17 16:10:07 -0300

    update bogo by adding delay

---
+++ JS/11exercisesofhell/bogosort.mjs
@@ -0,0 +1,41 @@
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms)) //Delay function to increase success rate by making the randomizer more variable.
+const n = isNaN(process.argv[2]) ? 164253 : parseFloat(process.argv[2]) //We fetch the numbers to sort from the user using Node Arguments
+const nodelay = (process.argv[3].toLowerCase() === 'true') ? true : false //Nodelay bool in case we don't want the delay.
+let lis = [] //We make an empty list.
+for(let i in String(n)){
+  lis.push(String(n)[i]) //We turn the given input into a string and push each character into a list.
+}
+
+function checkIfSorted(list){
+  let last = -Infinity //-Infinity for the first number so the user can use whatever number they want.
+  for(let i in list){
+    if(list[i] < last){ return false } //If last is bigger than current, return false.
+    else{
+      last = list[i] //Else, set current number as last and continue down the list.
+    }
+  }
+  return true //If it goes through the list and finds nothing, return true.
+}
+
+function badShuffle(){
+  let index = lis.length //Set index to list lenght.
+  while(index!=0){ //While index is not zero...
+    let randIndex = Math.floor(Math.random()*index) //Get a random number based from the index.
+    index-- //Reduce index.
+    [lis[index], lis[randIndex]] = [lis[randIndex], lis[index]] //Replace item from index with item from random index.
+  }
+}
+
+let sorted = checkIfSorted(lis) //Check if list isn't sorted by default.
+let trynum = 0 //We gotta keep track of tries, no?
+
+while(!sorted){ //while not sorted
+  trynum++ //increase try number
+  console.log(`Unsorted! ${lis} (Attempt ${trynum})`) //log disappointment
+  if(!nodelay) await delay(45) //Wait 45ms 
+  badShuffle(lis) //shuffle the list
+  sorted = checkIfSorted(lis) //check if sorted
+}
+
+trynum++ //increase try number
+console.log(`Should be sorted! ${lis} (Attempt ${trynum})`) //log success
